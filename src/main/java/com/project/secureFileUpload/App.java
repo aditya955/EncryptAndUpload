f1f1c9450467601a54c57757264e9576d05708a2
Aditya <adityajha2103@gmail.com>
2025-01-19 21:17:54 +0530

    Added interactive mode

--- src/main/java/com/project/secureFileUpload/App.java
+++ src/main/java/com/project/secureFileUpload/App.java
@@ -1,6 +1,7 @@
 package com.project.secureFileUpload;
 
 import java.util.Base64;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import javax.crypto.SecretKey;
@@ -11,6 +12,8 @@ import com.project.secureFileUpload.encryptionAlgorighms.CipherInstance;
 import com.project.secureFileUpload.encryptionAlgorighms.IEncryption;
 import com.project.secureFileUpload.upload.GoogleDrive;
 import com.project.secureFileUpload.upload.IUpload;
+import com.google.api.services.drive.model.File;
+import java.util.List;
 
 public class App 
 {
@@ -45,7 +48,7 @@ public class App
         Options options = new Options();
 
         Option algOption = new Option("a", "algorithm", true, "algorithm to use for encryption");
-        algOption.setRequired(true);
+        algOption.setRequired(false);
         options.addOption(algOption);
 
         Option mode = new Option("m", "mode", true, "mode of operatoin to use for encryption (without algorithm name)");
@@ -100,6 +103,10 @@ public class App
         key.setRequired(false);
         options.addOption(key);
 
+        Option interactive = new Option(null, "interactive", false, "Interactive mode");
+        interactive.setRequired(false);
+        options.addOption(interactive);
+
         return options;
     }
 
@@ -112,7 +119,186 @@ public class App
             System.exit(0);
         }
 
-        // String algorithm = cmd.getOptionValue("algorithm");
+        boolean interactive = cmd.hasOption("interactive");
+
+        try {
+            if(interactive) {
+                this.interactiveMode();
+            } else {
+                this.nonInteractiveMode(cmd);
+            }
+        } catch(Exception e) {
+            System.out.println("[-] Some error occured while processing...\n");
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    private boolean isNumber(String str) {
+        return str.chars().allMatch( Character::isDigit );
+    }
+
+    private void interactiveMode() {
+        Scanner in = new Scanner(System.in);
+        int user_input;
+
+        this.interactiveHelp();
+        do {
+            System.out.print("> ");
+            try {
+                user_input = in.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("[-] Invalid input");
+                user_input = -1;
+                in.nextLine();
+                continue;
+            }  
+
+            switch (user_input) {
+            case 0:
+                System.out.println("Exiting...");
+                break;
+            case 1:
+                System.out.print("File to encrypt > ");
+                String fileToEncrypt = in.next();
+
+                System.out.print("Output file name > ");
+                String outputEncryptedFile = in.next();
+
+                System.out.print("Key size > ");
+                int keySize = in.nextInt();
+
+                System.out.print("Algorithm > ");
+                String algorithm = in.next();
+
+                System.out.print("Mode [n for none] > ");
+                String mode = in.next();
+
+                if(mode.strip().toLowerCase().equals("n")) {
+                    mode = null;
+                }
+
+
+                SecretKey secretKey = CipherInstance.getClassInstance(algorithm, mode).generateKey(keySize);
+                boolean status = encrypt(algorithm, mode, secretKey, fileToEncrypt, outputEncryptedFile, null);
+
+                
+                if(status) {
+                    System.out.println("[+] File encrypted successfully");
+                    System.out.println("Key: " + CipherInstance.getClassInstance(algorithm, mode).getKeyStr(secretKey));
+                } else {
+                    System.out.println("[-] Error encrypting the file");
+                }
+                break;
+            case 2:
+                System.out.print("File to decrypt > ");
+                String fileToDecrypt = in.next();
+
+                System.out.print("Output file name > ");
+                String outputDecryptedFile = in.next();
+
+                System.out.print("Key size > ");
+                keySize = in.nextInt();
+                
+                System.out.print("Algorithm > ");
+                algorithm = in.next();
+                
+                System.out.print("Mode [n for none] > ");
+                mode = in.next();
+
+                if(mode.strip().toLowerCase().equals("n")) {
+                    mode = null;
+                }
+                
+                
+                System.out.print("Key > ");
+                String key = in.next();
+                secretKey = CipherInstance.getClassInstance(algorithm, mode).generateKey(key);
+                
+                status = decrypt(algorithm, mode, secretKey, fileToDecrypt, outputDecryptedFile, null);
+                
+                System.out.println(status ? "[+] File decrypted successfully" : "[-] Error decrypting the file");
+                break;
+            case 3:
+                System.out.print("File to upload > ");
+                String fileToUpload = in.next();
+                String fileId = upload(fileToUpload);
+
+                if(fileId == null) {
+                    System.out.println("[-] Error uploading the file");
+                    break;
+                }
+
+                System.out.println("File ID: " + fileId);
+                break;
+            case 4:
+                List<File> files = drive.getAllFiles();
+                System.out.println("Sr No.\t\tFile Name\t\t\tFile ID");
+                for(int i = 0; i < files.size(); i++) {
+                    File file = files.get(i);
+                    System.out.println((i + 1) + "\t\t" + file.getName() + "\t\t\t" + file.getId());
+                }
+                System.out.print("File ID/Sr No. > ");
+                fileId = in.next();
+
+                if(isNumber(fileId)) {
+                    int id = Integer.parseInt(fileId);
+                    if(id > 0 && id <= files.size()) {
+                        fileId = files.get(Integer.parseInt(fileId) - 1).getId();
+                    }
+                }
+
+                boolean found = false;
+                for(File file: files) {
+                    if(file.getId().equals(fileId)) {
+                        found = true;
+                        break;
+                    }
+                }
+
+                if(!found) {
+                    System.out.println("[-] Invalid file ID/Sr No.");
+                    break;
+                }
+
+                System.out.print("Output file name > ");
+                outputDecryptedFile = in.next();
+
+                status = download(fileId, outputDecryptedFile);
+                System.out.println(status ? "[+] File downloaded successfully" : "[-] Error downloading the file");
+                break;
+            case 5:
+                files = drive.getAllFiles();
+                System.out.println("Sr No.\t\tFile Name\t\t\tFile ID");
+                for(int i = 0; i < files.size(); i++) {
+                    File file = files.get(i);
+                    System.out.println((i + 1) + "\t\t" + file.getName() + "\t\t\t" + file.getId());
+                }
+                break;
+            case 99:
+                this.interactiveHelp();
+                break;
+            default:
+                System.out.println("Invalid Option");
+                this.interactiveHelp();
+            }
+        } while(user_input != 0);
+
+        in.close();
+    }
+
+    private void interactiveHelp() {
+        String help = "0. Exit\n" +
+                      "1. Encrypt\n" +
+                      "2. Decrypt\n" +
+                      "3. Upload\n" +
+                      "4. Download\n" +
+                      "5. List files\n" +
+                      "99. Help\n";
+        System.out.println(help);
+    }
+
+    private void nonInteractiveMode(CommandLine cmd) {
         String algorithm = cmd.getOptionValue("algorithm");
         String mode = cmd.getOptionValue("mode");
         String combinedBytes = cmd.getOptionValue("combinedBytes");
@@ -125,9 +311,15 @@ public class App
         String decrypt = cmd.getOptionValue("decrypt");
         String key = cmd.getOptionValue("key");
 
+        if(algorithm == null) {
+            System.out.println("[-] Algorithm not provided");
+            printHelp(defineOptions());
+            System.exit(1);
+        }
+
         // TODO: Implement these
-        String encryptAndUpload = cmd.getOptionValue("encryptAndUpload");
-        String downloadAndDecrypt = cmd.getOptionValue("downloadAndDecrypt");
+        // String encryptAndUpload = cmd.getOptionValue("encryptAndUpload");
+        // String downloadAndDecrypt = cmd.getOptionValue("downloadAndDecrypt");
 
         Scanner in = new Scanner(System.in);
         if(encrypt != null) {
