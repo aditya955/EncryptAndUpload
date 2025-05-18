596b325014f827a21118c4429868fe5659b38019
Aditya <adityajha2103@gmail.com>
2025-05-19 00:45:26 +0530

    Update: AesTest to take AES defaults from generic EncryptionTypes enum instead of directly accessing default method from AesDefaults

--- src/test/java/com/eau/EncryptAndUpload/encryptTest/AesTest.java
+++ src/test/java/com/eau/EncryptAndUpload/encryptTest/AesTest.java
@@ -4,19 +4,19 @@ import org.junit.jupiter.api.Test;
 
 import com.eau.EncryptAndUpload.builder.encryptor.EncryptorConfigBuilder;
 import com.eau.EncryptAndUpload.encrypt.AESEncryptor;
-import com.eau.EncryptAndUpload.presets.encrypt.AESDefault;
+import com.eau.EncryptAndUpload.enums.EncryptionTypes;
 
 public class AesTest {
     @Test
     public void aes128Test() {
-        EncryptorConfigBuilder builder = new EncryptorConfigBuilder(AESDefault.getAes128Default());
+        EncryptorConfigBuilder builder = new EncryptorConfigBuilder(EncryptionTypes.AES128.getDefaults());
         AESEncryptor aes = new AESEncryptor(builder);
         EncryptionTest.testEncryptionAndDecryption(aes, "AES128");
     }
     
     @Test
     public void aes256Test() {
-        EncryptorConfigBuilder builder = new EncryptorConfigBuilder(AESDefault.getAes256Default());
+        EncryptorConfigBuilder builder = new EncryptorConfigBuilder(EncryptionTypes.AES256.getDefaults());
         AESEncryptor aes = new AESEncryptor(builder);
         EncryptionTest.testEncryptionAndDecryption(aes, "AES256");
     }
